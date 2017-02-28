9ad369f0aa7f2386d2f66eab4222a299c7423c2c
pranaypaine <pranay.kumar.rocks@gmail.com>
2017-02-28 22:35:23 +0530

    final changes and completetion

--- js/main.js
+++ js/main.js
@@ -9,24 +9,39 @@ $(document).ready(function(){
 				client_secret:'71e610879de7db9e2cd9d7c1b2a91cde5f45d0e8'
 			}
 		}).done(function(user){
-			// if (user.error) {
-			// 	$('#profile').html(`
-			// 		<div class="panel panel-default">
-			// 		<div class="panel-heading">
-			// 		<h3 class="panel-title">User Not Found</h3>
-			// 		</div>
-			// 			<div class="panel-body">
-			// 				<div class="row">
-			// 				<div class="col-md-3">
-			// 				<img class="thumbnail avatar" src="images/git.png">
-			// 			</div>
-			// 			<div class="col-md-9">
-			// 			</div>
-			// 		</div>
-			// 		</div>
-			// 		</div>`);
-			// }
-			// else{
+			$.ajax({
+				url : 'https://api.github.com/users/'+username+'/repos',
+			data:{
+				client_id:'3712796d8b1de4fd3893',
+				client_secret:'71e610879de7db9e2cd9d7c1b2a91cde5f45d0e8',
+				sort: 'created: asc',
+				per_page: 5
+			}	
+			}).done(function(repos){
+				$.each(repos, function(index, repo){
+					$('#repos').append(`
+							<div class="well">
+							<div class="row">
+								<div class="col-md-7">
+									<strong>${repo.name}</strong> : ${repo.description}
+									<br>
+									<span class="label label-success">Language : ${repo.language}</span>
+								</div>
+								<div class="col-md-3">
+									<span class="label label-default">Forks : ${repo.forks_count}</span>
+									<span class="label label-primary">Watchers : ${repo.watchers_count}</span>
+									<span class="label label-success">Stars : ${repo.stargazers_count}</span>		
+
+								</div>
+								<div class="col-md-2">
+									<a href="${repo.html_url}" target="_blank" class="btn btn-primary">View Repo</a>
+								</div>
+							</div>
+							</div>
+						`);
+				});
+			});
+
 				$('#profile').html(`
 				<div class="panel panel-default">
 				<div class="panel-heading">
@@ -36,15 +51,31 @@ $(document).ready(function(){
 				<div class="row">
 					<div class="col-md-3">
 					<img class="thumbnail avatar" src="${user.avatar_url}">
+					<a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
 					</div>
 					<div class="col-md-9">
+						<span class="label label-default">Public Repos : ${user.public_repos}</span>
+						<span class="label label-primary">Public Gists : ${user.public_gists}</span>
+						<span class="label label-success">Followers : ${user.followers}</span>
+						<span class="label label-info">Following : ${user.following}</span>
+					<br><br>
+					<ul class="list-group">
+						<li class="list-group-item">Company : ${user.company}</li>
+						<li class="list-group-item">Website/Blog : <a href="${user.blog}" target=_blank>${user.blog}</a></li>
+						<li class="list-group-item">Location : ${user.location}</li>
+						<li class="list-group-item">Bio : ${user.bio}</li>
+						<li class="list-group-item">Member Since : ${user.created_at}</li>
+					</ul>
 					</div>
 				</div>
 				</div>
 				</div>
+				<h3 class="page-header">Latest Repos</h3>
+				<div id="repos"></div>
 				`);
-			// }
+
 		});
 	});
 });
-// https://api.github.com/users/pranaypaine
+
+//https://api.github.com/users/pranaypaine
